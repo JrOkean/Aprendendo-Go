7c0af0d66ce32c96df5d60836aa3b14860980b3c
Joana dos Santos Raposo <74430816+JrOkean@users.noreply.github.com>
2024-02-26 20:20:41 -0300

    resposta 1

--- exercicio-nível#1/um.go
+++ exercicio-nível#1/um.go
@@ -1,10 +1,13 @@
 package main
 
-import (
-	"fmt"
-)
+import "fmt"
 
 func main() {
-	fmt.Println("Hello, World!")
-}
+	x, y, z := 42, "James Bond", true
 
+	fmt.Println(x, "\n", y, "\n", z)
+	fmt.Println("-------- opção 2:")
+	fmt.Println(x)
+	fmt.Println(y)
+	fmt.Println(z)
+}
